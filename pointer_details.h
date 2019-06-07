f87b1ded1f54a5f489d5cb1693e3be0ee72e005d
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-06-07 01:56:32 -0700

    Define constructor

--- pointer_details.h
+++ pointer_details.h
@@ -6,6 +6,11 @@ class ptrDetails {
         bool isArray;
         unsigned arraySize;
         ptrDetails(T *ptr, unsigned size = 0) {
-
+            refCount = 1;
+            memPtr = ptr;
+            if(size > 0)
+                isArray = true;
+            else
+                isAttay = false;
         };
 }
