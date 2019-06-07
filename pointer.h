a8014c20fe945884b6ab6c1351ab1395b1c9f5e7
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-06-07 16:16:57 -0700

    Declare and Define pointer constructor

--- pointer.h
+++ pointer.h
@@ -4,3 +4,90 @@
 #include <cstdlib>
 #include "pointer_details.h"
 #include "iterator.h"
+
+/*
+    Pointer implements a pointer type that uses
+    garbage collection to release unused memory.
+    A Pointer must only be used to point to memory
+    that was dynamically allocated using new.
+    When used to refer to an allocated array,
+    specify the array size.
+*/
+template <class T, int size = 0>
+class Pointer{
+private:
+    // refContainer maintains the garbage collection list.
+    static std::list<PtrDetails<T> > refContainer;
+    // addr points to the allocated memory to which
+    // this Pointer pointer currently points.
+    T *addr;
+    /*  isArray is true if this Pointer points
+        to an allocated array. It is false
+        otherwise. 
+    */
+    bool isArray; 
+    // true if pointing to array
+    // If this Pointer is pointing to an allocated
+    // array, then arraySize contains its size.
+    unsigned arraySize; // size of the array
+    static bool first; // true when first Pointer is created
+    // Return an iterator to pointer details in refContainer.
+    typename std::list<PtrDetails<T> >::iterator findPtrInfo(T *ptr);
+public:
+    // Define an iterator type for Pointer<T>.
+    typedef Iter<T> GCiterator;
+    // Empty constructor
+    // NOTE: templates aren't able to have prototypes with default arguments
+    // this is why constructor is designed like this:
+    Pointer(){
+        Pointer(NULL);
+    }
+    Pointer(T*);
+    // Copy constructor.
+    Pointer(const Pointer &);
+    // Destructor for Pointer.
+    ~Pointer();
+    // Collect garbage. Returns true if at least
+    // one object was freed.
+    static bool collect();
+    // Overload assignment of pointer to Pointer.
+    T *operator=(T *t);
+    // Overload assignment of Pointer to Pointer.
+    Pointer &operator=(Pointer &rv);
+    // Return a reference to the object pointed
+    // to by this Pointer.
+    T &operator*(){
+        return *addr;
+    }
+    // Return the address being pointed to.
+    T *operator->() { return addr; }
+    // Return a reference to the object at the
+    // index specified by i.
+    T &operator[](int i){ return addr[i];}
+    // Conversion function to T *.
+    operator T *() { return addr; }
+    // Return an Iter to the start of the allocated memory.
+    Iter<T> begin(){
+        int _size;
+        if (isArray)
+            _size = arraySize;
+        else
+            _size = 1;
+        return Iter<T>(addr, addr, addr + _size);
+    }
+    // Return an Iter to one past the end of an allocated array.
+    Iter<T> end(){
+        int _size;
+        if (isArray)
+            _size = arraySize;
+        else
+            _size = 1;
+        return Iter<T>(addr + _size, addr, addr + _size);
+    }
+    // Return the size of refContainer for this type of Pointer.
+    static int refContainerSize() { return refContainer.size(); }
+    // A utility function that displays refContainer.
+    static void showlist();
+    // Clear refContainer when program exits.
+    static void shutdown();
+};
