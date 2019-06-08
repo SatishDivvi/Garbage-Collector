c84aabd4e40dd77c45814631c61aef8ea236812d
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-06-07 23:04:16 -0700

    Rename refCount to refcount

--- pointer_details.h
+++ pointer_details.h
@@ -4,12 +4,12 @@
 template <class T>
 class PtrDetails {
     public:
-        unsigned refCount;
+        unsigned refcount;
         T *memPtr;
         bool isArray;
         unsigned arraySize;
         PtrDetails(T *ptr, unsigned size = 0) {
-            refCount = 1;
+            refcount = 1;
             memPtr = ptr;
             arraySize = size;
             if(arraySize > 0)
